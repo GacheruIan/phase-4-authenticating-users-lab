287d5a1b74cfa41a49b6c6752a31d25db9766f96
Gacheruian <ian.gacheru@student.moringaschool.com>
2022-12-01 08:04:31 +0300

    Done assignment

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
   resources :articles, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+
+  #login
+  post '/login', to: "sessions#create"
+  #route for user stay logged in
+  get '/me', to: "users#show"
+  #logout
+  delete "/logout", to: "sessions#destroy"
+
+
+
 end
